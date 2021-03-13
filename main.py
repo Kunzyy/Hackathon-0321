c6566f1f01dbb4c520ad872eb597c5950d94ce37
Kunzy <nkuntz@scarlet.be>
2021-03-13 15:16:23 +0100

    Kivy

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
 print('Hello world !')
+
+
+class LoginScreen(GridLayout):
+    def __init__(self, **var_args):
+        super(LoginScreen, self).__init__(**var_args)
+
+
+class MyApp(App):
+    def build(self):
+        # return a LoginScreen() as a root widget
+        return LoginScreen()
+
+
+if __name__ == '__main__':
+    MyApp().run()
